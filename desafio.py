7fd12a73173863c2f71cce2803f5fa18c8189769
Giovane Fortuna <Fortuna_giovane47@hotmail.com>
2025-04-14 21:00:43 -0300

    melhorando funcionalidades na funcao depositado

--- desafio.py
+++ desafio.py
@@ -1,75 +1,100 @@
-print("Sistema do ZanBank")
-print("Selecione umas das opções")
+print("Bem-Vindo ao Sistema do ZanBank")
 
-print("[1] - Sacar")
-print("[2] - Depositar")
-print("[3] - Exibir o extrato")
-print("[7] - Sair " )
+dic_usuarios =  {}
+
+while True:
+    
+    resposta = input(("Deseja criar uma conta? S/N"))
+
+    if resposta == 's':
+        nome = input(("Digite o seu nome: "))
+        cpf = input(("Digite o seu cpf: "))
+        dic_usuarios["nome"] = nome
+        dic_usuarios["cpf"] = cpf
+        break
+
+    elif resposta == "n":
+        print("Precisa se cadastrar no sistema. ")
+    else:
+        print("Opção inválida: ")
+
+   
+print(dic_usuarios)
+
+
+
+print("Selecione umas das opções")
+print("[1] - Criar uma conta corrente")
+print("[2] - Sacar")
+print("[3] - Depositar")
+print("[4] - Exibir o extrato")
+print("[7] - Sair ")
   
 saldo_bancario = 500
 
-limites_para_saques = 3
+limites_para_saques = 10
 
 saques_realizados = 0
 
 extrato = []
 
-saldo_bancario_atualizado = 0
+conta_corrente = []
 
+saldo_atualizado = 0
 
 def sacar():
-    global saldo_bancario, limites_para_saques  
+    global limites_para_saques, saldo_atualizado
     
     valor_saque = int(input("Digite um valor para sacar: "))
 
-    if valor_saque > saldo_bancario:
+    if valor_saque > saldo_bancario and limites_para_saques < 0:
         print("Saldo insuficiente.")
-    elif limites_para_saques <= 0:
         print("Limite de saques diários atingido.")
-    else:
-        saldo_bancario -= valor_saque
+    elif valor_saque < saldo_bancario:
+        saldo_atualizado = saldo_bancario - valor_saque 
         limites_para_saques -= 1
         extrato.append(f"Saque: R${valor_saque:.2f}")
         print(f"Saque de R${valor_saque:.2f} realizado com sucesso!")
-        print(f"Saldo atualizado: R${saldo_bancario:.2f}")
+        print(f"Saldo atualizado: R${saldo_atualizado:.2f}")
         print(f"Saques restantes hoje: {limites_para_saques}")
 
+def depositar(): 
+    global saldo_atualizado
 
-def depositar():
     valor_depositado = int(input("Digite um valor para depositar: "))
-    print("Deposito realizado com sucesso")
-   
     
-    saldo_bancario_atualizado = saldo_bancario + valor_depositado
-    extrato.append(f"Depósito: R${valor_depositado:.2f}")
-    print(f"Depósito de R${valor_depositado:.2f} realizado com sucesso!")
-    print(f"O valor do saldo é {saldo_bancario_atualizado}")
+    saldo_atualizado_depositado = saldo_atualizado + valor_depositado 
+    print(saldo_atualizado_depositado)   
+
 
 def exibirExtrato():
    
-    print("\n--- Extrato Bancário ---")
+    print("\n---Extrato Bancário---")
 
     if extrato == 0:
         print(f"O extrato {transacao}")
     else:
         for transacao in extrato:
             print(transacao)
-                   
 
 while True:
 
+
     opcao = int(input("Escolha uma opção: "))
 
+    if opcao == 1:
+       
+        print("Conta Corrente Criada ")
 
-    if opcao == 1: 
+    elif opcao == 2: 
         sacar()
-    
-    elif opcao == 2:
+
+    elif opcao == 3:
         depositar()
         print("Depositado")
         
 
-    elif opcao == 3:
+    elif opcao == 4:
         exibirExtrato()
         print("Exibindo o extrato")
         
